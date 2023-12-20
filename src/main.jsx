08217af865eb855c5cdfb3a41b4307b1e1070365
Alex <alex@alex-dev.me>
2023-12-20 10:02:19 -0600

    Update import statement for styles from CSS to SCSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { JournalApp } from "./JournalApp";
-import "./styles.css";
+import "./styles.scss";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
