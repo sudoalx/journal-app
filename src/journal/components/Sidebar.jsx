08ec2e39008bbc68f5e061d5926fd171fab40b41
Alex <alex@alex-dev.me>
2023-12-21 14:09:04 -0600

    Add Sidebar component

---
+++ src/journal/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { Box, Divider, Drawer, Toolbar, Typography } from "@mui/material";
+
+export const Sidebar = ({ drawerWidth }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth, flexShrink: { sm: 0 } } }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { xs: "none", sm: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Alex
+          </Typography>
+        </Toolbar>
+        <Divider />
+      </Drawer>
+    </Box>
+  );
+};
