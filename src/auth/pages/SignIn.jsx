9d40111dc39a99641d7fdb361d9a46ba97f1154b
Alex <alex@alex-dev.me>
2023-12-20 19:25:33 -0600

    Add sign-in functionality and UI enhancements

--- src/auth/pages/SignIn.jsx
+++ src/auth/pages/SignIn.jsx
@@ -1,3 +1,120 @@
+import { Login, Google, Visibility, VisibilityOff } from "@mui/icons-material";
+import { Button, ButtonGroup, Grid, Link, TextField } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { useState } from "react";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const SignIn = () => {
-  return <div>SignIn</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <AuthLayout title="Sign In">
+      <Grid
+        item
+        sx={{
+          color: "primary.main",
+          fontSize: "1.5rem",
+          fontWeight: "bold",
+        }}
+      >
+        <form>
+          {/* email field */}
+          <TextField
+            label="Email"
+            placeholder="Enter your email"
+            type="email"
+            fullWidth
+            variant="outlined"
+          />
+          {/* password field */}
+          <TextField
+            label="Password"
+            placeholder="Enter your password"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            variant="outlined"
+            sx={{
+              marginTop: 2,
+            }}
+            InputProps={{
+              // show/hide password icon
+              endAdornment: !showPassword ? (
+                <VisibilityOff
+                  onClick={handleClickShowPassword}
+                  sx={{
+                    cursor: "pointer",
+                  }}
+                />
+              ) : (
+                <Visibility
+                  onClick={handleClickShowPassword}
+                  sx={{
+                    cursor: "pointer",
+                  }}
+                />
+              ),
+            }}
+          />
+          {/* Forgot Password? and Sign Up buttons */}
+          <Grid
+            container
+            direction={{ xs: "column", sm: "row" }}
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{
+              marginTop: 2,
+            }}
+          >
+            <Grid item>
+              <ButtonGroup variant="text">
+                <Button>Forgot Password?</Button>
+
+                <Link component={RouterLink} to="/auth/signup">
+                  <Button>Sign Up</Button>
+                </Link>
+              </ButtonGroup>
+            </Grid>
+            <Grid item>
+              <Button variant="contained">
+                <Login sx={{ marginRight: 1 }} />
+                Sign In
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+        {/* Divider */}
+        <Grid
+          item
+          sx={{
+            marginTop: 2,
+            backgroundColor: "primary.main",
+          }}
+        >
+          <hr
+            style={{
+              width: "100%",
+              height: "1px",
+              border: "none",
+            }}
+          />
+        </Grid>
+        {/* Login with Google button */}
+        <Grid
+          item
+          sx={{
+            marginTop: 2,
+          }}
+        >
+          <Button variant="contained" fullWidth>
+            <Google sx={{ marginRight: 1 }} />
+            Sign In with Google
+          </Button>
+        </Grid>
+      </Grid>
+    </AuthLayout>
+  );
 };
