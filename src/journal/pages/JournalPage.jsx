8895025e52c17e0ba764ea7f88753871a8c1c954
Alex <alex@alex-dev.me>
2023-12-20 19:25:37 -0600

    Add MailLock icon to JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { Typography } from "@mui/material";
-import { MailOutline } from "@mui/icons-material";
+import { MailOutline, MailLock } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
     <>
       <Typography variant={"h1"}>JournalPage</Typography>
       <MailOutline />
+      <MailLock />
     </>
   );
 };
