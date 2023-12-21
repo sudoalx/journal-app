4753bf55e76da82485517aaf2a1948de08091d01
Alex <alex@alex-dev.me>
2023-12-21 10:17:06 -0600

    Refactor SignUp component form layout

--- src/auth/pages/SignUp.jsx
+++ src/auth/pages/SignUp.jsx
@@ -17,147 +17,142 @@ export const SignUp = () => {
   };
   return (
     <AuthLayout title="Sign Up">
+      <form>
+        {/* name field */}
+        <TextField
+          label="Name"
+          placeholder="Enter your name"
+          autoComplete="name"
+          fullWidth
+          variant="outlined"
+          sx={{
+            marginTop: 2,
+          }}
+        />
+        {/* email field */}
+        <TextField
+          label="Email"
+          placeholder="Enter your email"
+          autoComplete="email"
+          type="email"
+          fullWidth
+          variant="outlined"
+          sx={{
+            marginTop: 2,
+          }}
+        />
+        {/* password field */}
+        <TextField
+          label="Password"
+          placeholder="Enter your password"
+          type={showPassword ? "text" : "password"}
+          autoComplete="new-password"
+          fullWidth
+          variant="outlined"
+          sx={{
+            marginTop: 2,
+          }}
+          InputProps={{
+            // show/hide password icon
+            endAdornment: !showPassword ? (
+              <VisibilityOff
+                onClick={handleClickShowPassword}
+                sx={{
+                  cursor: "pointer",
+                }}
+              />
+            ) : (
+              <Visibility
+                onClick={handleClickShowPassword}
+                sx={{
+                  cursor: "pointer",
+                }}
+              />
+            ),
+          }}
+        />
+        {/* confirm password field */}
+        <TextField
+          label="Password"
+          placeholder="Enter your password"
+          type={showPassword ? "text" : "password"}
+          autoComplete="new-password"
+          fullWidth
+          variant="outlined"
+          sx={{
+            marginTop: 2,
+          }}
+          InputProps={{
+            // show/hide password icon
+            endAdornment: !showPassword ? (
+              <VisibilityOff
+                onClick={handleClickShowPassword}
+                sx={{
+                  cursor: "pointer",
+                }}
+              />
+            ) : (
+              <Visibility
+                onClick={handleClickShowPassword}
+                sx={{
+                  cursor: "pointer",
+                }}
+              />
+            ),
+          }}
+        />
+        {/* other links */}
+        <Grid
+          container
+          direction={{ xs: "column", sm: "row" }}
+          justifyContent="end"
+          alignItems="center"
+          sx={{
+            marginTop: 2,
+          }}
+        >
+          <Grid item marginRight={1}>
+            <ButtonGroup variant="text">
+              <Link component={RouterLink} to="/auth/signin">
+                <Button>Aleady have an account?</Button>
+              </Link>
+            </ButtonGroup>
+          </Grid>
+          <Grid item>
+            <Button variant="contained">
+              <AppRegistration sx={{ marginRight: 1 }} />
+              Sign Up
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+      {/* Divider */}
       <Grid
         item
         sx={{
-          color: "primary.main",
-          fontSize: "1.5rem",
-          fontWeight: "bold",
+          marginTop: 2,
+          backgroundColor: "primary.main",
         }}
       >
-        <form>
-          {/* name field */}
-          <TextField
-            label="Name"
-            placeholder="Enter your name"
-            fullWidth
-            variant="outlined"
-            sx={{
-              marginTop: 2,
-            }}
-          />
-          {/* email field */}
-          <TextField
-            label="Email"
-            placeholder="Enter your email"
-            type="email"
-            fullWidth
-            variant="outlined"
-            sx={{
-              marginTop: 2,
-            }}
-          />
-          {/* password field */}
-          <TextField
-            label="Password"
-            placeholder="Enter your password"
-            type={showPassword ? "text" : "password"}
-            fullWidth
-            variant="outlined"
-            sx={{
-              marginTop: 2,
-            }}
-            InputProps={{
-              // show/hide password icon
-              endAdornment: !showPassword ? (
-                <VisibilityOff
-                  onClick={handleClickShowPassword}
-                  sx={{
-                    cursor: "pointer",
-                  }}
-                />
-              ) : (
-                <Visibility
-                  onClick={handleClickShowPassword}
-                  sx={{
-                    cursor: "pointer",
-                  }}
-                />
-              ),
-            }}
-          />
-          {/* confirm password field */}
-          <TextField
-            label="Password"
-            placeholder="Enter your password"
-            type={showPassword ? "text" : "password"}
-            fullWidth
-            variant="outlined"
-            sx={{
-              marginTop: 2,
-            }}
-            InputProps={{
-              // show/hide password icon
-              endAdornment: !showPassword ? (
-                <VisibilityOff
-                  onClick={handleClickShowPassword}
-                  sx={{
-                    cursor: "pointer",
-                  }}
-                />
-              ) : (
-                <Visibility
-                  onClick={handleClickShowPassword}
-                  sx={{
-                    cursor: "pointer",
-                  }}
-                />
-              ),
-            }}
-          />
-          {/* other links */}
-          <Grid
-            container
-            direction={{ xs: "column", sm: "row" }}
-            justifyContent="end"
-            alignItems="center"
-            sx={{
-              marginTop: 2,
-            }}
-          >
-            <Grid item marginRight={1}>
-              <ButtonGroup variant="text">
-                <Link component={RouterLink} to="/auth/signin">
-                  <Button>Aleady have an account?</Button>
-                </Link>
-              </ButtonGroup>
-            </Grid>
-            <Grid item>
-              <Button variant="contained">
-                <AppRegistration sx={{ marginRight: 1 }} />
-                Sign Up
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-        {/* Divider */}
-        <Grid
-          item
-          sx={{
-            marginTop: 2,
-            backgroundColor: "primary.main",
+        <hr
+          style={{
+            width: "100%",
+            height: "1px",
+            border: "none",
           }}
-        >
-          <hr
-            style={{
-              width: "100%",
-              height: "1px",
-              border: "none",
-            }}
-          />
-        </Grid>
-        {/* Login with Google button */}
-        <Grid
-          item
-          sx={{
-            marginTop: 2,
-          }}
-        >
-          <Button variant="contained" fullWidth>
-            <Google sx={{ marginRight: 1 }} />
-            Sign Up with Google
-          </Button>
-        </Grid>
+        />
+      </Grid>
+      {/* Login with Google button */}
+      <Grid
+        item
+        sx={{
+          marginTop: 2,
+        }}
+      >
+        <Button variant="contained" fullWidth>
+          <Google sx={{ marginRight: 1 }} />
+          Sign Up with Google
+        </Button>
       </Grid>
     </AuthLayout>
   );
