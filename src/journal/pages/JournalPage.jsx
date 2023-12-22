6a95cbddc6c2b949d0c89b9d1862ee008ac9d282
Alex <alex@alex-dev.me>
2023-12-22 01:32:03 -0600

    Add NothingSelected component to JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,17 +1,11 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelected } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero minima
-        dolores ut eveniet sit adipisci repudiandae hic beatae obcaecati et
-        assumenda ratione pariatur, magni sed facilis quidem. Facilis, quaerat
-        quidem!
-      </Typography>
-
-      {/* Nothing selected */}
+      <NothingSelected />
       {/* Note view */}
     </JournalLayout>
   );
