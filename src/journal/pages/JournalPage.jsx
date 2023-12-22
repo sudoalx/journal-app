4b68eb998edc6897585b18b30d73b767bc44cf4f
Alex <alex@alex-dev.me>
2023-12-22 11:26:28 -0600

    Add note creation button to JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,31 @@
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelected } from "../views";
+import { NoteView, NothingSelected } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       <NothingSelected />
-      {/* Note view */}
+      {/* <NoteView /> */}
+      <IconButton
+        size="large"
+        sx={{
+          color: "text.primary",
+          transition: "all 0.3s ease",
+          backgroundColor: "primary.main",
+          ":hover": {
+            backgroundColor: "primary.dark",
+          },
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          borderRadius: "50%",
+          boxShadow: 2,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
