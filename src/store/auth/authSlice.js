a077313274f17472e728bb90f3b2bd3c5131c4c9
Alex <alex@alex-dev.me>
2024-01-05 17:29:13 -0600

    Refactor authSlice.js to handle login and logout actions

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -13,9 +13,21 @@ export const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-        login: (state, action) => {
+        login: (state, { payload }) => {
+            state.status = 'authenticated';
+            state.uuid = payload.uuid
+            state.email = payload.email
+            state.displayName = payload.displayName
+            state.photoURL = payload.photoURL
+            state.errorMessage = null;
         },
-        logout: (state, payload) => {
+        logout: (state, { payload }) => {
+            state.status = 'not-authenticated';
+            state.uuid = null;
+            state.email = null;
+            state.displayName = null;
+            state.photoURL = null;
+            state.errorMessage = payload.errorMessage;
         },
         checkAuth: (state) => {
             state.status = 'checking';
