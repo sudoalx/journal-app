daaa0b2d174fcfc1172a0db2830d8ec915a2aa1a
Alex <alex@alex-dev.me>
2024-01-05 17:29:18 -0600

    Refactor auth thunks to use signInWithGoogle and handle login/logout

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,13 +1,18 @@
-import { authSlice, checkAuth } from "./authSlice";
+import { signInWithGoogle } from "../../firebase/providers";
+import { checkAuth, login, logout } from "./";
 
-export const checkingAuth = (email, password) => async (dispatch) => {
+export const checkingAuth = (email, password) => {
     return async (dispatch) => {
         dispatch(checkAuth());
     }
 }
 
-export const startGoogleLogin = () => async (dispatch) => {
+export const startGoogleLogin = () => {
     return async (dispatch) => {
         dispatch(checkAuth());
+        const result = await signInWithGoogle();
+        if (!result.ok) return dispatch(logout({ errorMessage: result.errorMessage }));
+
+        dispatch(login(result))
     }
 }
