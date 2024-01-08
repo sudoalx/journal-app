cc11d4ccfc1691311d5f5640df4ebcb563e319be
Alex <alex@alex-dev.me>
2024-01-08 01:23:32 -0600

    Add authentication state handling to AppRouter.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { useSelector } from "react-redux";
+import { CheckingAuth } from "../ui/";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "../firebase/config";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../store/auth";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, []);
+
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
+
   return (
     <Routes>
       <Route path="/auth/*" element={<AuthRoutes />} />
