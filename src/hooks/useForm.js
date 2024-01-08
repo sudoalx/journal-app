663370cfb23993b3d64d8955d0915a2379c0110f
Alex <alex@alex-dev.me>
2024-01-08 14:04:31 -0600

    Add useEffect to set initial form state
    This commit adds a new useEffect hook to set the initial form state in the useForm.js file.

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -10,6 +10,10 @@ export const useForm = (initialForm = {}, formValidations = {}) => {
     useEffect(() => {
         createValidators()
     }, [formState])
+    useEffect(() => {
+        setFormState(initialForm)
+    }, [initialForm])
+
 
     const isFormValid = useMemo(() => {
 
