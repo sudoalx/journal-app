1ca4fa8dd518566fed464b91e39ac656f09fd933
Alex <alex@alex-dev.me>
2024-01-08 11:49:50 -0600

    Add helper functions for loading notes and start loading notes on user login

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { FirebaseAuth } from "../firebase/config";
 import { login, logout } from "../store/auth";
+import { startLoadingNotes } from "../store/journal/thunks";
 
 
 export const useCheckAuth = () => {
@@ -14,6 +15,7 @@ export const useCheckAuth = () => {
             if (!user) return dispatch(logout());
             const { uid, email, displayName, photoURL } = user;
             dispatch(login({ uid, email, displayName, photoURL }));
+            dispatch(startLoadingNotes());
         });
     }, []);
 
